7af64330758f6c5b8f94588affc511f0c46da37c
sanjiv001 <84955095+sanjiv001@users.noreply.github.com>
2023-11-07 22:12:17 +0545

    'finished'

--- lib/view/searchapi.dart
+++ lib/view/searchapi.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:recipeapp/repository/food_repository.dart';
+import 'package:recipeapp/view/detailscreen.dart';
 
 class SearchScreen extends SearchDelegate {
  FoodRecipeRepository _foodList = FoodRecipeRepository();
@@ -42,26 +43,58 @@ class SearchScreen extends SearchDelegate {
               ),
             );
           } else {
-            return ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: Row(
-                      children: [
-                        Container(
-                          height: 120,
-                          width: 120,
-                          child: CachedNetworkImage(
-                              imageUrl: data[index].image.toString()),
+            return Expanded(
+              child: data.isNotEmpty
+                  ?ListView.builder(
+                              itemCount: data.length,
+                              itemBuilder: (context, index) {
+                                return Card(
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        height: 120,
+                                        width: 120,
+                                        child: InkWell(
+                                          onTap: () {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        DetailScreen(
+                                                            id: snapshot.data[index].id,
+                                                            title: snapshot.data[index].title.toString(),
+                                                            image: snapshot.data[index].image.toString(),
+                                                            imageType:
+                                                            snapshot.data[index].imageType.toString(),
+                                                            calories: snapshot.data[index].calories,
+                                                            protein: snapshot.data[index].protein.toString(),
+                                                            fat: snapshot.data[index].fat.toString(),
+                                                            carbs: snapshot.data[index].carbs.toString())));
+                                          },
+                                          child: CachedNetworkImage(
+                                              imageUrl:
+                                                  data[index].image.toString()),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      Flexible(
+                                          child: Text(
+                                              data[index].title.toString())),
+                                    ],
+                                  ),
+                                );
+                              })  :  const  Flexible(
+                    child:Center(
+                      child:  Text(
+                          'No results found Please try with diffrent search',
+                          style: TextStyle(fontSize: 24),
                         ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        Text(data[index].title.toString()),
-                      ],
                     ),
-                  );
-                });
+                  ),
+
+            );
           }
         });
   }
